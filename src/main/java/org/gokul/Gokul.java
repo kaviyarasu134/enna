df104a2c8ac55a8a7965e16df81dcbb2cf9850ed
kaviyarasu134 <kaviyarasuravi5@gmail.com>
2024-05-18 14:25:06 +0530

    so waht

---
+++ src/main/java/org/gokul/Gokul.java
@@ -0,0 +1,20 @@
+package org.gokul;
+
+public class Gokul {
+	
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	
+	
+	private void sysout() {
+		
+		
+		System.out.println("she");
+		
+
+	}
+
+}
